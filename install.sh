203826d4ec3546d45bd26edd19a0c454d19b495e
Brahma Dev <me@brahma.world>
2019-08-28 17:07:57 +0530

    Added FiraCode as default font

--- install.sh
+++ install.sh
@@ -10,6 +10,30 @@ function is_mac(){
   ( is_platform Darwin ) && return 0 || return 1
 }
 
+function install_firacode(){
+	fonts_dir="${HOME}/.local/share/fonts"
+	if [ ! -d "${fonts_dir}" ]; then
+		echo "mkdir -p $fonts_dir"
+		mkdir -p "${fonts_dir}"
+	else
+		echo "Found fonts dir $fonts_dir"
+	fi
+
+	for type in Bold Light Medium Regular Retina; do
+		file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
+		file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
+		if [ ! -e "${file_path}" ]; then
+			echo "wget -O $file_path $file_url"
+			wget -O "${file_path}" "${file_url}"
+		else
+		echo "Found existing file $file_path"
+		fi;
+	done
+
+	echo "fc-cache -f"
+	fc-cache -f
+}
+
 function install_powerline_fonts(){
 	# clone
 	git clone https://github.com/powerline/fonts.git --depth=1
@@ -44,6 +68,7 @@ function install_vscode(){
 	
 	cd $HOME
 
+	install_firacode
 	install_powerline_fonts
 }
 
